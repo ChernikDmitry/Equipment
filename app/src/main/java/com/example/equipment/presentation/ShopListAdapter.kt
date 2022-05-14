e043748283d8e04c33132f7b3b4bd235b83e1202
Dima <tamstroy@gmail.com>
2022-05-14 23:52:36 +0300

    change notifyDataSetChange to DiffCallback

--- app/src/main/java/com/example/equipment/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/equipment/presentation/ShopListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.equipment.R
@@ -15,16 +16,17 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopList,value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
     var onShopItemLongClickListener:((ShopItem)->Unit)? = null
     var onShopItemClickListener:((ShopItem)->Unit)? = null
-    var onShopItemSwipeListener: ((ShopItem)->Unit)? = null
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        Log.d("ShopListAdapter", "onCreateViewHolder, count: ${++count}")
+
         val layout = when (viewType){
             VIEW_TYPE_ENABLED->R.layout.item_shop_enabled
             VIEW_TYPE_DISABLED->R.layout.item_shop_disabled
@@ -39,6 +41,7 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        Log.d("ShopListAdapter", "onBindViewHolder, count: ${++count}")
         val shopItem = shopList[position]
         viewHolder.tvName.text = shopItem.name
         viewHolder.tvCount.text = shopItem.count.toString()
@@ -78,10 +81,6 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
 //    interface OnShopItemLongClickListener{
 //        fun onShopItemLongClick(shopItem: ShopItem)
 //    }
-    interface ItemTouchHelperAdapter{
-        fun onItemDismiss(position: Int)
-    }
-
 
 
     companion object{
